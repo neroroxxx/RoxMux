95274275005c8a807d71b46e314e4ec8da882b77
Nero <neroroxxx@gmail.com>
2022-12-03 12:48:43 -0500

    added ANAMUX example

--- examples/ANAMUX/ANAMUX.ino
+++ examples/ANAMUX/ANAMUX.ino
@@ -1,17 +1,16 @@
 /*
   https://www.RoxXxtar.com/bmc
   Example to read 2 74HC4067
-  The Rox74HC40XX has 3 classes, a base Rox74HC40XX and 2 children:
-
-  Rox74HC4067, this is a 16 pin analog-in multiplexer
-  Rox74HC4051, this is a 8 pin analog-in multiplexer
+  
+  For use with ANAMUX
 */
 
 #include <RoxMux.h>
 
-// MUX_TOTAL is the number of 74HC4067s that you have chained together
-// if you have more than 1 then change it to that number.
-#define MUX_TOTAL 1
+// ANAMUX has 2 74HC4067 multiplexers so the mux total is 2
+// if you have more that 1 ANAMUX wired then you want to increase
+// MUX_TOTAL by 2 for each additional ANAMUX
+#define MUX_TOTAL 2
 Rox74HC4067 <MUX_TOTAL> mux;
 
 
@@ -20,8 +19,9 @@ Rox74HC4067 <MUX_TOTAL> mux;
 #define PIN_S2  21
 #define PIN_S3  20
 #define PIN_SIG0  19
+#define PIN_SIG1  18
 
-// this is the pin of the 74HC4067 that we will read
+// this is the pin of the ANAMUX that we will read
 #define PIN_TO_READ 0
 
 // keep track of the previous value, while the library handles 10-bit values
@@ -37,6 +37,8 @@ void setup(){
   // set what pin is the signal pin for the first mux
   // the first argument is the index of the mux, second is the pin number
   mux.setSignalPin(0, PIN_SIG0);
+  // second mux signal pin
+  mux.setSignalPin(1, PIN_SIG1);
 }
 
 void loop(){
@@ -54,7 +56,7 @@ void loop(){
       uint8_t data = mux.read(i)>>2;
       if(data!=prev){
         prev = data;
-        Serial.print("Mux pin ");
+        Serial.print("ANAMUX pin ");
         Serial.print(i);
         Serial.print(" = ");
         Serial.println(data);
